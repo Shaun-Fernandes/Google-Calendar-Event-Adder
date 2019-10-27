a37152d563def30862a3fb4b4ba9551bd69e5df5
Shaun <shaunferns99@gmail.com>
2019-10-27 23:23:07 +0400

    Edit Files

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# Google-Calender-Event-Adder
+# Google-Calendar-Event-Adder
 
-Finds your events from an excel file and automatically creates Google Calendar Events with appropriate name and date
+Finds the days you've been scheduled from an excel file and automatically creates Google Calendar Events with the appropriate event name and date
+
+
+## Getting Started
+
+* Go to this url: https://developers.google.com/calendar/quickstart/python?refresh=1
+* Click 'Enable The Google Calendar API'
+* Accept
+* Click Download client configurations
+* Copy the credentials.json file you just downloaded to this directory
+
+
+### Setting up the Program 
+This will only happen the first time you run the program. The program will ask for access to your google calendar, and create a token.pickle:
+* Run program: ```python main.py```
+* Sign in to google
+* QuickStart wants access to google account - Allow
+
+### Running the Program
+Copy an excel file with the month's schedule to this folder
+
+Run the program with:
+```bash
+python main.py
+```
+
+When you run the program it will ask for 3 things:
+1. Name of the excel file: Enter the name exactly (capital letters, small letters and spaces should be taken care of)
+2. Sheet number: Each Excel file has multiple sheets. Enter the sheet number you want to scan. (eg:- 1, 2, 3, ...)
+3. Name: Enter the name which you want to search the file for
+
+
+##### Example:
+```
+$ python main.py
+Enter the name of the excel file: May Schedule.xlsx
+Which sheet number of the excel file do you want to scan [1, 2, 3, ...]: 1
+Enter the name you want to search in the excel file: Dave
+```
+
+* Run the program each time you want to scan a new excel sheet
+* By the end of it, all the google calendar events will be created for you.
+* Check [calendar.google.com](https://calendar.google.com/) or the calendar app on your phone to see the events.
+* Each event is set with a reminder notification for 6 hours, 1 day, 2 days, and 3 days before the event.
--- main.py
+++ main.py
@@ -17,18 +17,16 @@ def main():
     Gets event name and date from event_helper script,
     Uses Google API to create calander events.
     """
-    ################################
-    #       Variables to edit      #
-    ################################
+    service = get_service()
     Excel_file_path = input("Enter the name of the excel file: ")
     while not path.isfile(Excel_file_path):
         print("The file name entered does not exist!")
-        Excel_file_path = input("Enter the name of the excel file: ")
-    sheet_index = int(input("Which sheet number in the file to scan (starts from 0): "))
+        Excel_file_path = input("Enter the name of the excel file (make sure it is placed in the same folder): ")
+    sheet_index = int(input("Which sheet number in the file do you want to scan [1, 2, 3 etc.]: ")) - 1
+    key = input("Enter the name you want to search in the excel file: ")
 
     # Geting event data from the excel file
-    events = event_helper.get_events(Excel_file_path, sheet_index)
-    service = get_service()
+    events = event_helper.get_events(Excel_file_path, sheet_index, key)
 
     # Printing, creating and uploading events
     for eve in events:
@@ -49,7 +47,7 @@ def main():
                     {'method': 'popup', 'minutes': 6 * 60},         # 6 hours
                     {'method': 'popup', 'minutes': 1 * 24 * 60},    # 1 day
                     {'method': 'popup', 'minutes': 2 * 24 * 60},    # 2 days
-                    {'method': 'popup', 'minutes': 4 * 24 * 60}     # 4 days
+                    {'method': 'popup', 'minutes': 3 * 24 * 60}     # 3 days
                 ],
             },
             'colorId': '3'
@@ -60,7 +58,7 @@ def main():
         print("Event Link:", format(event.get('htmlLink')))
 
     if len(events) == 0:
-        print("No events found in this sheet.\nTry changing value of 'sheet_index'")
+        print("No events found in this sheet.\nTry changing value of 'sheet index'")
 
 def get_service():
     creds = None
@@ -76,6 +74,12 @@ def get_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            if not os.path.exists('credentials.json'):
+                print("\nCreate a credientials.json file and copy it to this folder.")
+                print("\nhttps://developers.google.com/calendar/quickstart/python")
+                print("Go to this site, click the Enable Google Calander API button, and copy the downloaded credientials.json file to this folder")
+                input("Press any key to end the program")
+                exit()
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server()
@@ -89,6 +93,7 @@ def get_service():
 
 if __name__ == '__main__':
     main()
+    input("Press any key to end the program")
 
 
 # 41beb2e09877fadffac346651f3c78a4429b7433
